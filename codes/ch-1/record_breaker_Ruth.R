23c058a58b311dcc0718e1a9e34c67fbf7759e84
TheHBK <37610177+TheHBK@users.noreply.github.com>
2019-08-31 23:31:00 +0530

    Added references details to the Maris simulation

--- codes/ch-1/record_breaker_Ruth.R
+++ codes/ch-1/record_breaker_Ruth.R
@@ -1,9 +1,14 @@
 ##############################################################
 ### Simulating Moris'chance of beating the record of Babe Ruth
 ##############################################################
+# The probability of home run has been provided in the book The Drunkard's Walk
 prob.home.run <- 1/14.7
+
+# The data about the plate appearance has been taken from 
+# https://www.baseball-reference.com/teams/NYY/1961.shtml
 plate.appearance <- 698
-num.Moris <- 10000
+
+num.Moris <- 10000 # Number of Moris type player simulations we want to do.
 home.run.vec <- replicate(num.Moris, sum(rbinom(n=698, size = 1, prob = prob.home.run)))
 record.breach <- sum(home.run.vec>60)
 print(record.breach)
