1eec02e5b42c6cb0fd3ff26c76f8477ea71e553b
TheHBK <37610177+TheHBK@users.noreply.github.com>
2019-09-08 02:12:23 +0530

    Monty-Hall problem

--- codes/ch-2/monty_hall_strategy.R
+++ codes/ch-2/monty_hall_strategy.R
@@ -1,19 +1,17 @@
 ##########################################################################
 ### Simulating the Monty-Hall problem's results for the strategy to change
 ##########################################################################
-behind.door <- c("Car", "Goat", "Goat")
-# The function that gives unusual output
-generate.behind.door <- function(num.samples) {a <- replicate(n = num.samples, sample(behind.door, replace = FALSE)); return(a)}
+behind.door <- c("Car", "Goat", "Goat") # The things behind the doors.
 
-# This gives expected output
-sample.prizes <- function() { v <- sample(behind.door, replace = FALSE); return(v)}
-generate.behind.door1 <- function(num.samples) {a <- replicate(n = num.samples, sample.prizes()); return(a)}
-select.position <- function() {a <- sample(c(1,2,3), size = 1); return(a)}
+sample.prizes <- function() { v <- sample(behind.door, replace = FALSE); return(v)} # Sample the things -- just randomize the order.
+generate.behind.door <- function(num.samples) {a <- replicate(n = num.samples, sample.prizes()); return(a)} # Generate many samples of the things behing the door.
+select.door <- function() {a <- sample(c(1,2,3), size = 1); return(a)} # Select one of the doors.
 
-show.goat <- function(selected.position, behind.doors) {
+#' Show the goat every time -- exactly as the anchor does.
+show.goat <- function(selected.door, things.behind.door) {
   positions <- c(1,2,3)
-  remaining.positions <- positions[-c(selected.position)]
-  goat.position <- remaining.positions[which(behind.doors[remaining.positions] == "Goat")]
+  remaining.positions <- positions[-c(selected.door)]
+  goat.position <- remaining.positions[which(things.behind.door[remaining.positions] == "Goat")]
   if (length(goat.position) == 1) {
     return(goat.position)
   } else {
@@ -22,23 +20,25 @@ show.goat <- function(selected.position, behind.doors) {
   }
 }
 
-strategy.switch <- function(goat.shown, behind.doors, selected.position) {
-  selected <- behind.doors[-c(selected.position,goat.shown)]
+#' The switch strategy.
+strategy.switch <- function(goat.shown, things.behind.door, selected.door) {
+  selected <- things.behind.door[-c(selected.door,goat.shown)]
   return(selected)
 }
 
-strategy.not.switch <- function(goat.shown, behind.doors, selected.position) {
-  selected <- behind.doors[selected.position]
+strategy.not.switch <- function(goat.shown, things.behind.door, selected.door) {
+  selected <- things.behind.door[selected.door]
   return(selected)
 }
 
+#' See how the strategy performs. The simulations must be consistent with the theory.
 test.strategy <- function(num.sim) {
   success <- rep(0, num.sim)
   for (i in 1:num.sim) {
-    selected.position = select.position()
-    behind.doors <- generate.behind.door1(num.samples = 1)
-    goat.shown <- show.goat(selected.position, behind.doors)
-    car.or.goat <- strategy.switch(goat.shown, behind.doors, selected.position)
+    selected.door = select.door()
+    things.behind.door <- generate.behind.door(num.samples = 1)
+    goat.shown <- show.goat(selected.door, things.behind.door)
+    car.or.goat <- strategy.switch(goat.shown, things.behind.door, selected.door)
     if (car.or.goat == "Car") {
       success[i] <- 1
     }
@@ -47,8 +47,8 @@ test.strategy <- function(num.sim) {
   prob.success <- success.num/num.sim
   return(prob.success)
 }
-
-prob.success <- test.strategy(1000000)
+num.sim <- 1000000 # Higher number  leads to very near result to the theoretical answer.
+prob.success <- test.strategy(num.sim=num.sim)
 print(prob.success)
 
 
