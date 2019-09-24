a13695d7983ea4e97d9b62f86d38eb5c98a3059a
TheHBK <37610177+TheHBK@users.noreply.github.com>
2019-09-24 23:39:27 +0530

    German lottery from chapter 4

---
+++ codes/ch-4/germanLottery.R
@@ -0,0 +1,26 @@
+# Simulate German lottery from chapter-4, The Drunkard's Walk.
+
+draw.lottery <- function(num.draws) {
+  lottery.numbers <- seq(1, 49)
+  lottery.draws <- matrix(nrow = num.draws, ncol = 6)
+  for (i in 1:num.draws) {
+    lottery.draws[i,] <- sample(lottery.numbers, 6, replace = TRUE)
+  }
+  return(lottery.draws)
+}
+
+
+do.experiment <- function(num.sim, num.draws) {
+  repeat.vec <- rep(0, num.sim)
+  for (i in 1:num.sim) {
+    lottery.draws <- draw.lottery(num.draws = num.draws)
+    repeat.vec[i] <- sum(duplicated(lottery.draws))
+    # duplicated.lottery.draws <- lottery.draws[duplicated(lottery.draws) | duplicated(lottery.draws, fromLast=TRUE)]
+  }
+  return(repeat.vec)
+}
+
+repeat.vec <- do.experiment(num.sim = 10000, num.draws = 30160)
+sum(repeat.vec)
+prob <- sum(repeat.vec)/length(repeat.vec)
+prob
